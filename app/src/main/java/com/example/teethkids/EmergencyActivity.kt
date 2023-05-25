6e56c5fc798d52f103b43e1c8d02d06c9a4b24d5
AlvaroWRP <62357109+AlvaroWRP@users.noreply.github.com>
2023-05-25 00:03:37 -0300

    recyclerview (incompleto)

--- app/src/main/java/com/example/teethkids/EmergencyActivity.kt
+++ app/src/main/java/com/example/teethkids/EmergencyActivity.kt
@@ -1,12 +1,10 @@
 package com.example.teethkids
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.teethkids.databinding.EmergencyActivityBinding
-import com.example.teethkids.databinding.LogInActivityBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class EmergencyActivity : AppCompatActivity() {
 
@@ -39,28 +37,7 @@ class EmergencyActivity : AppCompatActivity() {
 
         }
 
-        /*
-
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.page_2 -> {
-                    // Respond to navigation item 1 click
-                    true
-                }
-                R.id.page_3 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                R.id.page_4 -> {
-                    val pass = Intent(this, ProfileActivity::class.java)
-                    startActivity(pass)
-                    true
-                }
-                else -> false
-            }
-
-         */
-        }
+    }
 
     private fun replaceFragment(fragment : Fragment){
 
