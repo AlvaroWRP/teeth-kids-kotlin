dcdd540bee3bc0a6a34cf0601f9129177a562c20
AlvaroWRP <62357109+AlvaroWRP@users.noreply.github.com>
2023-06-05 18:11:56 -0300

    Tela de Profile finalmente funcional

--- app/src/main/java/com/example/teethkids/Profile.kt
+++ app/src/main/java/com/example/teethkids/Profile.kt
@@ -1,120 +1,168 @@
 package com.example.teethkids
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.FileProvider
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.example.teethkids.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import java.io.File
-import java.io.IOException
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.example.teethkids.databinding.FragmentProfileBinding
+import java.io.ByteArrayOutputStream
 
 class Profile : Fragment() {
-    private lateinit var binding: FragmentProfileBinding
-    private lateinit var cameraExecutor: ExecutorService
-    private lateinit var firestore: FirebaseFirestore
-    private lateinit var currentUserID: String
-    private val uid = FirebaseAuth.getInstance().currentUser?.uid
-    private lateinit var photoPath: String
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
-    private var REQUEST_TAKE_PHOTO = 1
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storageRef: StorageReference
+    private lateinit var activity: Activity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = requireActivity()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        firestore = FirebaseFirestore.getInstance()
-        cameraExecutor = Executors.newSingleThreadExecutor()
 
-        getCurrentUserID()
+        firestore = FirebaseFirestore.getInstance()
+        storageRef = FirebaseStorage.getInstance().reference
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        currentUser?.let {
+            val userId = it.uid
+
+            val userRef = firestore.collection("users").document(userId)
+            userRef.get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val name = document.getString("name")
+                        val phone = document.getString("phone")
+                        val bio = document.getString("biography")
+
+                        binding.textViewName.text = name
+                        binding.textViewPhone.text = phone
+                        binding.textViewBio.text = bio
+                    } else {
+                        Log.d(TAG, "User document does not exist")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e(TAG, "Error getting user document: ${exception.message}", exception)
+                }
+        }
 
         binding.btnTakePicture.setOnClickListener {
-            takePicture()
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openCamera()
+            } else {
+                requestCameraPermission()
+            }
         }
-
-
     }
 
-    //Pega o ID do usuario atual
-    private fun getCurrentUserID() {
-        val currentUserDocumentRef = firestore.collection("users").document("current_user")
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.CAMERA),
+            REQUEST_CODE_PERMISSIONS
+        )
+    }
 
-        currentUserDocumentRef.get()
-            .addOnSuccessListener { documentSnapshot ->
-                if (documentSnapshot.exists()) {
-                    currentUserID = documentSnapshot.getString("userID") ?: ""
-                } else {
-                    // Documento nao existe
-                }
-            }
-            .addOnFailureListener {
-                // Falha ao pegar o ID do usuario
-            }
-
-        val userDocument = firestore.collection("users").document(uid!!)
-        userDocument.get().addOnSuccessListener { document ->
-            val name = document.getString("name")
-            val phone = document.getString("phone")
-            val biography = document.getString("biography")
-
-            // Atualiza os dados do usuario na UI
-            binding.tvName.text = name
-            binding.tvPhone.text = phone
-            binding.tvBio.text = biography
-        }
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_CODE_CAMERA)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK){
-            binding.imgProfile.setImageURI(Uri.parse(photoPath))
-        }
-    }
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap?
+            imageBitmap?.let {
+                // Update the ImageView with the captured image
+                binding.imageViewProfile.setImageBitmap(imageBitmap)
 
-    private fun takePicture(){
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val pm = requireActivity().packageManager
+                val currentUser = FirebaseAuth.getInstance().currentUser
+                currentUser?.let {
+                    val userId = it.uid
+                    val profilePicRef = storageRef.child("profile_pictures/$userId")
 
-        if (intent.resolveActivity(pm)!= null){
-            var photoFile: File? = null
-            try {
-                photoFile = createImageFile()
-            }catch (_: IOException){}
-            if (photoFile != null){
+                    val baos = ByteArrayOutputStream()
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                    val imageData: ByteArray = baos.toByteArray()
 
-                val photoUri = activity?.let { FileProvider.getUriForFile(it, "com.example.teethkids", photoFile) }
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-                startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+                    val uploadTask: UploadTask = profilePicRef.putBytes(imageData)
+                    uploadTask.continueWithTask { task ->
+                        if (!task.isSuccessful) {
+                            task.exception?.let {
+                                throw it
+                            }
+                        }
+                        profilePicRef.downloadUrl
+                    }.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val downloadUri: Uri? = task.result
+                            val profilePicUrl = downloadUri.toString()
+
+                            val userRef = firestore.collection("users").document(userId)
+                            userRef.update("profilePicUrl", profilePicUrl)
+                                .addOnSuccessListener {
+                                    Log.d(TAG, "Profile picture URL updated successfully")
+                                }
+                                .addOnFailureListener { exception ->
+                                    Log.e(
+                                        TAG,
+                                        "Error updating profile picture URL: ${exception.message}",
+                                        exception
+                                    )
+                                }
+                        }
+                    }
+                }
             }
         }
     }
-    private fun createImageFile(): File? {
-        val fileName = "Photo"
-        val storageDir = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            fileName,
-            ".jpg",
-            storageDir
-        )
-        photoPath = image.absolutePath
-        return image
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val TAG = "ProfileFragment"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val REQUEST_CODE_CAMERA = 100
     }
 }
+
+
+
+
+
